204fcc337b44d8fb6cec8af41f8e797db087d247
Marlon Erler <viridian035@gmail.com>
2025-12-28 21:48:41 +0100

    setup project

---
+++ sunrise/sunrise/sunriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunriseApp.swift
+//  sunrise
+//
+//  Created by vd on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct sunriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
